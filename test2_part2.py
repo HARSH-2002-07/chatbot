34bef12c6cc562ce7c19ccd243b654fd18e32086
HARSH-2002-07 <hj075028@gmail.com>
2025-03-11 22:57:29 +0530

    Added some more features in test2 part2

--- test2_part2.py
+++ test2_part2.py
@@ -22,3 +22,77 @@ import subprocess
 import threading
 import json
 import os
+
+env_vars = dotenv_values(".env")
+Username = env_vars("Username")
+Assistantname = env_vars("Assistantname")
+DefaultMessage = f'''{Username} : Hello {Assistantname}, How are you?
+{Assistantname} : Welcome {Username}. I am doing well. How may i help you?'''
+subprocess = []
+Functions = ["open", "close", "play", "system", "content", "google search", "youtube search"]
+
+def ShowDefaultChatifNoChats():
+    File = open(r'Data\ChatLog.json', "r", encoding='utf-8')
+    if len(File.read())<5:
+        with open(TempDirectoryPath('Database.data'), 'w', encoding='utf-8') as file:
+            file.write("")
+
+        with open(TempDirectoryPath('Response.data'), 'w', 'utf-8') as file:
+            file.write(DefaultMessage)
+
+def ReadChatLogJson():
+    with open(r'Data\ChatLog.json', "r", encoding='utf-8') as file:
+        chatlog_data = json.load(file)
+    return chatlog_data
+
+def ChatLogIntegration():
+    json_data = ReadChatLogJson()
+    formatted_chatlog = ""
+    for entry in json_data:
+        if entry["role"] == "user":
+            formatted_chatlog += f"User: {entry['content']}\n"
+        elif entry["role"] == "assistant":
+            formatted_chatlog += f"Assistant: {entry['content']}\n"
+    formatted_chatlog = formatted_chatlog.replace("User", Username + " ")
+    formatted_chatlog = formatted_chatlog.replace("Assistant", Assistantname + " ")
+
+    with open(TempDirectoryPath('Database.data'), 'w', encoding='utf-8') as file:
+            file.write(AnswerModifier(formatted_chatlog))
+
+def ShowChatsOnGUI():
+    File = open(TempDirectoryPath('Database.data'), "r", encoding='utf-8')
+    Data = File.read()
+    if len(str(Data))>0:
+        lines = Data.split('\n')
+        result = '\n'.join(lines)
+        File.close()
+        File = open(TempDirectoryPath('Response.data'), "w", 'utf-8')
+        File.write(result)
+        File.close()
+
+def InitialExecution():
+    SetMicrophoneStatus("False")
+    ShowTextToScreen("")
+    ShowDefaultChatifNoChats()
+    ChatLogIntegration()
+    ShowChatsOnGUI()
+
+InitialExecution()
+
+def MainExecution():
+    TaskExecution = False
+    ImageExecution = False
+    ImageGenerationQuery = ""
+
+    SetAssistantStatus("Listening...")
+    Query = SpeechRecognition()
+    ShowTextToScreen(f"{Username} : {Query}")
+    SetAssistantStatus("Thinking...")
+    Decision = FirstLayerDMM(Query)
+
+    print("")
+    print(f"Decision : {Decision}")
+    print("")
+
+    G = any([i for i in Decision if i.startswith("general")])
+    R = any([i for i in Decision if i.startswith("realtime")])
