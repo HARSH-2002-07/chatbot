57340a27208f3de9d717b250806a62d75acfd3d6
HARSH-2002-07 <hj075028@gmail.com>
2025-03-11 10:57:40 +0530

    Fix: Made it faster in generating general responses

--- Brain.py
+++ Brain.py
@@ -1,53 +1,72 @@
+from groq import Groq
+from json import load, dump
+import datetime
+from dotenv import dotenv_values
+
+env_vars = dotenv_values(".env")
+
+Username = env_vars.get("Username")
+Assistantname = env_vars.get("Assistantname")
+GroqAPIKey = env_vars.get("GroqAPIKey")
+
+client = Groq(api_key=GroqAPIKey)
+
+System = f"""Hello, I am {Username}, You are a very accurate and advanced AI chatbot named {Assistantname} which also has real-time up-to-date information from the internet.
+*** Do not tell time until I ask, do not talk too much, just answer the question.***
+*** Reply in only English, even if the question is in Hindi, reply in English.***
+*** Do not provide notes in the output, just answer the question and never mention your training data. ***
 """
-Install the Google AI Python SDK
 
-$ pip install google-generativeai
-"""
-import os
-import google.generativeai as genai
-from dotenv import load_dotenv
+SystemChatBot = [
+    {"role": "system", "content": System}
+]
 
-# Load environment variables from .env file
-load_dotenv()
+def RealTimeInformation():
+  current_data_time = datetime.datetime.now()
+  day = current_data_time.strftime("%A")
+  date = current_data_time.strftime("%d")
+  month = current_data_time.strftime("%B")
+  year = current_data_time.strftime("%Y")
+  hour = current_data_time.strftime("%H")
+  minute = current_data_time.strftime("%M")
+  second = current_data_time.strftime("%S")
 
-# Get API key from environment variable
-api_key = os.getenv("GOOGLE_API_KEY")
+  data = f"please use this realtime information if needed, \n"
+  data += f"Day: {day}\nDate: {date}\nMonth: {month}\nYear: {year}\n"
+  data += f"Time: {hour} hours :{minute} minutes :{second} seconds.\n"
+  return data
 
-genai.configure(api_key=api_key)
+def AnswerModifier(Answer):
+    lines = Answer.split('\n')
+    non_empty_lines = [line for line in lines if line.strip()]
+    return '\n'.join(non_empty_lines)
 
-# Create the model
-generation_config = {
-  "temperature": 1,
-  "top_p": 0.95,
-  "top_k": 64,
-  "max_output_tokens": 8192,
-  "response_mime_type": "text/plain",
-}
+def ChatBot(Query):
+  SystemChatBot.append({"role": "user", "content": Query})
 
-model = genai.GenerativeModel(
-  model_name="gemini-1.5-pro",
-  generation_config=generation_config,
-  # safety_settings = Adjust safety settings
-  # See https://ai.google.dev/gemini-api/docs/safety-settings
-  system_instruction="you are a vitual assistant in a robotics lab in KIET group of institutions, you are professional assisstant",
-)
+  completion = client.chat.completions.create(
+      model="llama3-70b-8192",
+      messages=SystemChatBot + [{"role": "system", "content": RealTimeInformation()}],
+      max_tokens=1024,
+      temperature=0.7,
+      top_p=1,
+      stream=True,
+      stop=None
+   )
 
-history = []
-print("Hello, how can i help you?")
+  Answer = ""
 
-while True:
+  for chunk in completion:
+    if chunk.choices[0].delta.content:
+      Answer += chunk.choices[0].delta.content
 
-  user_input = input("You: ")
+  Answer = Answer.replace("</s>", "")
 
-  chat_session = model.start_chat(
-    history= history
-    )
+  SystemChatBot.append({"role": "assistant", "content": Answer.strip()})
 
-  response = chat_session.send_message(user_input)
+  return AnswerModifier(Answer)
 
-  model_response = response.text
-  print(f'Bot: {model_response}')
-  print()
-
-history.append({"role": "user", "parts": [user_input]})
-history.append({"role": "model", "parts": [model_response]})
+if __name__ == "__main__":
+    while True:
+        user_input = input("Enter Your Query: ")
+        print(ChatBot(user_input))
